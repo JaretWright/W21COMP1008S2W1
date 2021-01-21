982428b18f444aabb20c9bf6265228e3721eadc6
Jaret Wright <hkkA223>
2021-01-21 14:42:28 -0500

    Completed challenge 1

--- src/WarmUpExercises.java
+++ src/WarmUpExercises.java
@@ -3,5 +3,16 @@ public class WarmUpExercises {
         //challenge 1
         for (int i=100; i>=0 ;i-=5)
             System.out.println(i);
+
+        //challenge 2
+        System.out.println("\nChallenge 2 & 3");
+        for (int i=0; i<=100 ; i+=10)
+        {
+//            if (i%20==0)
+//                System.out.println(i + " Java");
+//            else
+//                System.out.println(i);
+            System.out.println(i%20==0?i + " Java":i);
+        }
     }
 }
